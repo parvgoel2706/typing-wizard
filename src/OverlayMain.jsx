4d1131ff6a0364e1c85a4af0b6dac2fd82bc960a
parvgoel2706 <parvgoel2345@gmail.com>
2025-03-10 23:47:07 +0530

    applying inline css

--- src/OverlayMain.jsx
+++ src/OverlayMain.jsx
@@ -14,14 +14,24 @@ export default function OverlayMain({
 
   useEffect(() => {
     if (Status === "start") {
-        setOverlayClass("dispMain");
+      setOverlayClass("dispMain");
     }
   }, [Status]);
 
   return (
     <div
       key={Status}
-      className={`overlayMain ${!isRunning ? overlayClass : "hideMain"}`}
+      className={`${!isRunning ? overlayClass : "hideMain"}`}
+      style={{
+        position: "absolute",
+        left: "0%",
+        transform: "translate(100%)",
+        top: "0",
+        display: "flex",
+        alignItems: "center",
+        width: "100vw",
+        height: "95vh",
+      }}
     >
       <Wizard />
       {Status === "start" && <StartPage setIsRunning={setIsRunning} />}
