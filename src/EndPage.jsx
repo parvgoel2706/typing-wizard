10edf7385fda5c0ae8f1909a07bb8e9794bcb0a5
parvgoel2706 <parvgoel2345@gmail.com>
2025-04-09 18:02:41 +0530

    adding disclaimer

--- src/EndPage.jsx
+++ src/EndPage.jsx
@@ -20,7 +20,7 @@ export default function EndPage({
 
   return (
     <div className="EndPage">
-      
+      <h2>Data(WPM and accuracy) will be displayed here</h2>
       <button onClick={handleReset}>Restart</button>
       <button onClick={handleHome}>Back to Home</button>
     </div>
